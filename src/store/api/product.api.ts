17b48ac6e541a21bda16e9709ac9864623f9c92f
DeveloperDolon <dolonr718@gmail.com>
2025-04-28 13:52:50 +0600

    pagination properties added

--- src/store/api/product.api.ts
+++ src/store/api/product.api.ts
@@ -1,9 +1,12 @@
+import { TPagination } from "../../types";
 import { baseApi } from "./baseApi";
 
 const productApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     productList: builder.query({
-      query: () => ({ url: "/products" }),
+      query: ({ limit, skip }: TPagination) => ({
+        url: `/products?limit=${limit}&skip=${skip}`,
+      }),
       providesTags: ["product"],
     }),
   }),
